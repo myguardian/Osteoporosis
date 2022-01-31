8bba33e1e61781b23742d0dd4cde6d4806400f7f
deolmoh <deolmoh@sheridancollege.ca>
2022-01-30 19:06:51 -0500

    Update deepLearning.py
    Ironed out other errors and fixed issues with losses

--- 3-Machine_Learning_Model/deepLearning.py
+++ 3-Machine_Learning_Model/deepLearning.py
@@ -1,14 +1,19 @@
 import logging
 import sys
+import os
+import math
+import pandas as pd
+import statistics
+import keras
 from keras import backend as K
 from pandas import read_csv
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
 
 def set_directory():
     # detect the current working directory and add the sub directory
@@ -25,18 +30,17 @@ def root_mean_squared_error(y_true, y_pred):
         return K.sqrt(K.mean(K.square(y_pred - y_true)))
         
 # Function to create model, required for KerasClassifier
-def create_model(layer1, layer2):
+def create_model(layer1, layer2 , dimensions):
     # create model
     model = Sequential()
-    model.add(Dense(30, input_dim=30, activation='relu'))
+    model.add(Dense(d, input_dim=d, activation='relu'))
     model.add(Dense(layer1, activation='relu'))
     model.add(Dense(layer2, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1))
     # Compile model
-    model.compile(loss='root_mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss=keras.losses.MeanAbsoluteError(), optimizer='adam', metrics=['accuracy'])
     return model
     
-
     
 def perform_deep_learning(path):
    
@@ -45,50 +49,54 @@ def perform_deep_learning(path):
         # Load the data from the CSV file and select the features
         data = pd.read_csv(path)
         features = list(data.columns.values)
+        features = features.remove('bmdtest_tscore_fn')
+        d = len(features)
         
+        X = data[features]
+        y = data['bmdtest_tscore_fn']
+
         # Split data into training and test sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=40)
         logging.info(f'Collecting data form the csv')
         
+        models = []
+    
+        # create model    
+        models.append(("Convolutional 30/50/50/1", create_model(50, 50, d)))
+        models.append(("Convolutional 30/100/100/1", create_model(100, 100, d)))
+        models.append(("Convolutional 30/500/500/1", create_model(500, 500, d)))
+            
+            
+        
     except ValueError as er:
         logging.error(er)
         logging.error('Unable to create open csv')
-
-    # create model    
-    models.add("Convolutional 30/50/50/1", KerasRegressor(build_fn=create_model(50, 50), epochs=50, batch_size=10, verbose=0))
-    models.add("Convolutional 30/100/100/1", KerasRegressor(build_fn=create_model(100, 100), epochs=50, batch_size=10, verbose=0))
-    models.add("Convolutional 30/500/500/1", KerasRegressor(build_fn=create_model(500, 500), epochs=50, batch_size=10, verbose=0))
     
     
     
-    f = open("deep_learning__results/RMSE_Results.txt", "x")
+    f = open("deep_learning_results/RMSE_Results.txt", 'w+')
     
     try:
         # evaluate each model in turn
         for name, model in models:   
            
-            estimators = []
-            estimators.append(('standardize', StandardScaler()))
-            estimators.append(('mlp', Model)
-            pipeline = Pipeline(estimators)
-            kfold = KFold(n_splits=10)
-            results = cross_val_score(pipeline, X, Y, cv=kfold)
-            f.write("%s Standardized: %.2f +-(%.2f) RMSE \n" % (name, results.mean(), results.std()))
-            
-            model.fit(X_train, y_train)
+            model.fit(X_train, y_train, epochs=100, verbose=0, validation_split = 0.2)
             predictions = model.predict(X_test)
             rsme = math.sqrt(metrics.mean_squared_error(y_test, predictions))
+
             f.write('%s: RSME: (%f) -  SI: (%f) \n\n' % (name, rsme, rsme/statistics.mean(y_train)))
             
-            f.close()
+            
             
             logging.info(f'Training %s' % (name))
+        f.close()
+        logging.info(f'Deep Leanrning Analysis Results Complete')
     except ValueError as er:
         logging.error(er)
         logging.error('Unable to create and train deep learning models')
-
-    logging.info(f'Deep Leanrign Analysis Results Complete')
+        f.close()
         
+ 
 if __name__ == "__main__":
 
     try:
