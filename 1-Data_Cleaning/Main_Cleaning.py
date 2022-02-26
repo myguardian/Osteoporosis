b960538d5fd614b25b6d75e74bed187dbdff4ce6
Alex Canales <alexcanales766@gmail.com>
2022-02-25 21:11:26 -0500

    Created new Data Cleaning Script

--- 1-Data_Cleaning/Main_Cleaning.py
+++ 1-Data_Cleaning/Main_Cleaning.py
@@ -1,5 +1,4 @@
 import logging
-
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -7,198 +6,50 @@ import sys
 import os
 import sys
 
-import sweetviz
-
+# Use logging commands instead of print
 logging.basicConfig(level=logging.INFO)
 
-# Create Data frame Variable
+# Create the global variables
 df = []
 
-# Load the different variables we want to use
-
+# Used when removing duplicate rows
 patient_id_col = ['PatientId']
 
-# 4 Columns are numerical
-numerical_col = ['PatientAge',
-                 'bmdtest_height',
-                 'bmdtest_weight',
-                 'bmdtest_tscore_fn']
+# We will fill null cells with mean
+numerical_cols = ['PatientAge',
+                  'bmdtest_height',
+                  'bmdtest_weight',
+                  'bmdtest_tscore_fn']
 
 # We will fill null cells with mode
-nominal_col = [
+nominal_cols = [
     'PatientGender',
     'parentbreak',
     'ptunsteady',
-    'whereliv',
-    'education',
     'alcohol',
     'wasfractdue2fall',
     'ptfall',
-    'fxworried',
-    'notworking',
-    'marital',
+    'oralster',
+    'smoke'
+    ]
 
-    'oralster', 'smoke']
-
-# We will fill null cells with 0 for these columns
-special_nominal = ['arthritis',
-                   'cancer',
-                   'diabetes',
-                   'heartdisease',
-                   'respdisease',
-                   'howbreak',
-                   'hip',
-                   'ankle',
-                   'clavicle',
-                   'elbow',
-                   'femur',
-                   'spine',
-                   'wrist',
-                   'shoulder',
-                   'tibfib', ]
-
-
-# __________________________________________________________
-
-# function to covert lbs to kg
-def lbs_to_kg(weight_value):
-    weightLb = weight_value
-    if weightLb is not None:
-        weightKg = weightLb * 0.45359237
-        return weightKg
-
-
-def bmi(height_value, weightKg):
-    # add a buffer to each weight to account for overweight individuals
-    buffer = 30
-
-    # for each weight, check if its greater than the BMI normal weight range, for the appopriate height interval
-    if height_value <= 152.4 and weightKg >= 56.7 + buffer:
-        # less than 5ft 0
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 152.4 < height_value <= 154.9 and weightKg >= 56.7 + buffer:
-        # 5ft0 to 5ft1
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 152.4 < height_value <= 157.5 and weightKg >= 59 + buffer:
-        # 5ft1 to 5ft2
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 157.5 < height_value <= 160 and weightKg >= 61.2 + buffer:
-        # 5ft2 < 5ft3
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 160 < height_value <= 162.6 and weightKg >= 63.5 + buffer:
-        # 5ft3 < 5ft4
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 162.6 < height_value <= 165.1 and weightKg >= 65.8 + buffer:
-        # 5ft4 < 5ft5
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 165.1 < height_value <= 167.6 and weightKg >= 68 + buffer:
-        # 5ft5 < 5ft6
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 167.6 < height_value <= 170.2 and weightKg >= 70.3 + buffer:
-        # 5ft6 < 5ft7
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 170.2 < height_value <= 172.7 and weightKg >= 72.6 + buffer:
-        # 5ft7 < 5ft8
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 172.7 < height_value <= 175.3 and weightKg >= 74.8 + buffer:
-        # 5ft8 < 5ft9
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 175.3 < height_value <= 177.8 and weightKg >= 77.1 + buffer:
-        # 5ft9 < 5ft10
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 177.8 < height_value <= 180.3 and weightKg >= 79.4 + buffer:
-        # 5ft10 < 5ft11
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 180.3 < height_value <= 182.9 and weightKg >= 81.6 + buffer:
-        # 5ft11 < 6ft0
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 182.9 < height_value <= 185.4 and weightKg >= 83.9 + buffer:
-        # 6ft0 < 6ft1
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 185.4 < height_value <= 188 and weightKg >= 86.2 + buffer:
-        # 6ft1 < 6ft2
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 188 < height_value <= 190.5 and weightKg >= 88.5 + buffer:
-        # 6ft2 < 6ft3
-        weightKg = lbs_to_kg(weightKg)
-
-    elif 190.5 < height_value and weightKg >= 90.7 + buffer:
-        # max range
-        # 6ft3 < 6ft4
-        weightKg = lbs_to_kg(weightKg)
-
-    return weightKg
-
-
-# Converting Values into Metric
-def data_to_metric(idx, height_value, weight_value):
-    try:
-
-        # Create variables to hold the values
-        heightCm = 0
-        weightKg = 0
-        metric = True
-
-        if 1 < height_value < 2.2:
-            # Convert METERS to CM
-            heightCm = height_value * 100
-
-        elif 50 < height_value < 84:
-            # Height value is too high to be METERS or FEET and too low to be CM.
-            # Assume the height is INCHES and convert to CM
-            heightIn = height_value
-            heightCm = heightIn * 2.54
-            metric = False  # we use this flag later with weight...
-
-        elif height_value > 125:
-            # The height is probably in CM
-            heightCm = height_value
-            metric = True
-
-        if metric:
-            # The assumption is that if units are missing, they still used the same units type (metric vs imperial)
-            # It's impossible to do the same inference as with height because the "reasonable value" ranges are less
-            # distinct.
-            weightKg = weight_value
-
-            doBmi = True
-            if doBmi:
-                weightKg = bmi(height_value, weightKg)
-            # check if the weight thats assumed to be kg, is actually lbs (ex 95 kg or 95 lbs)
-
-        else:
-            # data is imperial
-            # Convert data from Lbs to KGs
-            weightKg = lbs_to_kg(weight_value)
-
-        return heightCm, weightKg
-
-    except ValueError as err:
-        logging.error(err)
-        logging.error(f'Unable to convert height to metric for patient id = {idx}')
-
-
-def fill_zeros_in_height_weight_with_mean():
-    df['bmdtest_height'].replace(0, df['bmdtest_height'].mean(), inplace=True)
-    df['bmdtest_weight'].replace(0, df['bmdtest_weight'].mean(), inplace=True)
-
-
-# __________________________________________________________
+# We will fill null cells with 0
+special_nominal_cols = ['arthritis',
+                        'cancer',
+                        'diabetes',
+                        'heartdisease',
+                        'respdisease',
+                        'howbreak',
+                        'hip',
+                        'ankle',
+                        'clavicle',
+                        'elbow',
+                        'femur',
+                        'spine',
+                        'wrist',
+                        'shoulder',
+                        'tibfib'
+                        ]
 
 
 # Remove the duplicates using the Patient ID and Baseline ID. ID's are unique, meaning we shouldn't have duplicates
@@ -208,84 +59,140 @@ def remove_duplicates_with_id():
         df.reset_index(drop=True, inplace=True)
 
     except ValueError as er:
-        logging.error(er)
+        logging.error(str(er))
 
 
-# We are going to use the mean to fill in the values
+# Weight values are in kilograms
+def lbs_to_kg(weight_value):
+    if weight_value is not None:
+        weightKg = weight_value * 0.45359237
+        return weightKg
+
+
+# Height and Weight values are in kilograms
+def calculate_bmi(idx, height_value, weight_value):
+    try:
+        return weight_value / ((height_value / 100) ** 2)
+    except ValueError as er:
+        logging.error(str(er))
+        logging.error(f'Unable to calculate BMI for patient id = {idx}')
+
+
+# Converting Values into Metric
+def data_to_metric(idx, height_value, weight_value):
+    try:
+
+        heightCm = 0
+        weightKg = 0
+        isMetric = True  # Flag is used when checking if the weight is metric
+
+        # Lets convert the height
+        if 1 < height_value < 2.2:
+            heightCm = height_value * 100  # Convert METERS to CM
+
+        elif 50 < height_value < 84:
+            # Height value is too high to be METERS or FEET and too low to be CM.
+            # Assume the height is INCHES and convert to CM
+            heightCm = height_value * 2.54
+            isMetric = False
+
+        elif height_value > 125:
+            # The height is probably in CM
+            heightCm = height_value
+
+        # Lets convert the weight
+        if isMetric:
+            weightKg = weight_value
+        else:
+            weightKg = lbs_to_kg(weight_value)
+
+        return heightCm, weightKg
+
+    except ValueError as er:
+        logging.error(str(er))
+        logging.error(f'Unable to convert height and weight to metric for patient id = {idx}')
+
+
+def fill_bmi_with_mean():
+    try:
+        df['bmi'].replace(0, df['bmi'].mean(), inplace=True)
+        df['bmi'].fillna((df['bmi'].mean()), inplace=True)
+    except ValueError as er:
+        logging.error(str(er))
+
+
 def fill_numerical_with_mean():
-    for column in numerical_col:
+    for column in numerical_cols:
         try:
-            df[column].fillna(df[column].mean(), inplace=True)
+            mean = df[column].mean()
+            df[column].fillna(mean, inplace=True)
+            df[column].replace(0, mean, inplace=True)
         except ValueError as er:
-            logging.error(er)
+            logging.error(str(er))
 
 
-# Fill non-special columns with mode
+
 def fill_nominal_with_mode():
-    for column in nominal_col:
+    for column in nominal_cols:
         try:
-            df[column].fillna(df[column].mode()[0], inplace=True)
+            mode = df[column].mode()[0]
+            df[column].fillna(mode, inplace=True)
         except ValueError as er:
-            logging.error(er)
+            logging.error(str(er))
 
 
-# Fill special columns with 0
 def fill_special_nominal_with_zero():
-    for column in special_nominal:
+    for column in special_nominal_cols:
         try:
             df[column].fillna(0, inplace=True)
         except ValueError as er:
-            logging.error(er)
+            logging.error(str(er))
 
 
 if __name__ == "__main__":
-
     # Loading the data
     try:
+        logging.info(f'Loading File\n')
         file_name = sys.argv[1]
-        logging.info(f'Loading Data {file_name}\n')
         df = pd.read_csv(file_name)
 
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
 
-    # ----------------------------------------------------------------------
-    # Selecting all features required for the model building process
     try:
         logging.info("Selecting features from Data\n")
-        all_features = np.concatenate((patient_id_col, numerical_col, nominal_col, special_nominal))
-        df = df[all_features]
+        all_columns = np.concatenate((patient_id_col, numerical_cols, nominal_cols, special_nominal_cols))
+        df = df[all_columns]
 
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
 
     try:
-        logging.info(f'Creating feature data frame html graph')
-        analysis = sweetviz.analyze(df)
-        analysis.show_html('pre_analysis_results/pre_analysis.html', open_browser=False)
+        logging.info(f'Creating feature data frame html graph\n')
+        # analysis = sw.analyze(df)
+        # analysis.show_html('pre_analysis_results/pre_analysis.html', open_browser=False)
 
     except ValueError as e:
-        logging.error(e)
-        logging.error('Error showing feature report')
+        logging.error(str(e))
+        logging.error('Error showing feature report\n')
 
-    # ----------------------------------------------------------------------
-    # Dealing with Duplicates
     try:
-        logging.info("Removing duplicates.\n")
+        logging.info("Removing duplicates\n")
         remove_duplicates_with_id()
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
 
-    # ----------------------------------------------------------------------
-    # Converting values into metric
     try:
 
         logging.info('Converting Height and Weight to Metric\n')
+
         converted_data_tuple = [
-            data_to_metric(df.loc[idx, 'PatientId'], df.loc[idx, 'bmdtest_height'], df.loc[idx, 'bmdtest_weight'])
+            data_to_metric(df.loc[idx, 'PatientId'],
+                           df.loc[idx, 'bmdtest_height'],
+                           df.loc[idx, 'bmdtest_weight'])
             for idx
             in range(len(df))]
 
@@ -295,26 +202,47 @@ if __name__ == "__main__":
         # Get weights from tuple
         df['bmdtest_weight'] = [x[1] for x in converted_data_tuple]
 
-        fill_zeros_in_height_weight_with_mean()
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
 
-    # ----------------------------------------------------------------------
-    # Imputing values into missing cells
     try:
-        logging.info("Imputing Data into missing Columns\n")
-
+        logging.info("Imputing Data into numerical Columns\n")
         fill_numerical_with_mean()
-        fill_nominal_with_mode()
-        fill_special_nominal_with_zero()
-
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
 
-    # ----------------------------------------------------------------------
-    # Saving data to the CSV File
+    try:
+        logging.info("Imputing Data into nominal Columns\n")
+        fill_nominal_with_mode()
+    except ValueError as e:
+        logging.error(str(e))
+        quit()
+
+    try:
+        logging.info("Imputing Data into special nominal Columns\n")
+        fill_special_nominal_with_zero()
+    except ValueError as e:
+        logging.error(str(e))
+        quit()
+
+    try:
+        logging.info('Creating BMI column\n')
+
+        df['bmi'] = 0
+        df['bmi'] = [
+            calculate_bmi(df.loc[idx, 'PatientId'],
+                          df.loc[idx, 'bmdtest_height'],
+                          df.loc[idx, 'bmdtest_weight'])
+            for idx
+            in range(len(df))]
+
+        fill_bmi_with_mean()
+    except ValueError as e:
+        logging.error(str(e))
+        quit()
+
     try:
         logging.info('Saving Data to CSV file\n')
 
@@ -325,5 +253,5 @@ if __name__ == "__main__":
         logging.info(f'Data saved to {path}\n')
 
     except ValueError as e:
-        logging.error(e)
+        logging.error(str(e))
         quit()
