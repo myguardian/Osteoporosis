34c4a6db7f4c608b4c903c660858ea61019fe25e
Neil Sano <neil.hiroshi.sano@gmail.com>
2022-04-12 16:39:37 -0400

    Added the Catboost script to be run and changed the output location to Output

--- 3-Machine_Learning_Model/SciKit_Models.py
+++ 3-Machine_Learning_Model/SciKit_Models.py
@@ -5,48 +5,21 @@ import sys
 import logging
 
 
-def set_directory():
+def set_directory(temp_path):
     # detect the current working directory and add the sub directory
     main_path = os.getcwd()
-    absolute_path = main_path + "/models_results"
-    model1_path = absolute_path + "/scikit_model_1"
-    model2_path = absolute_path + "/scikit_model_2"
-    model3_path = absolute_path + "/scikit_model_3"
-    model4_path = absolute_path + "/scikit_model_4"
-    model1_waterfall = model1_path + "/waterfalls"
-    model2_waterfall = model2_path + "/waterfalls"
-    model3_waterfall = model3_path + "/waterfalls"
-    model4_waterfall = model4_path + "/waterfalls"
+    absolute_path = main_path + temp_path
     try:
-        if not os.path.exists(absolute_path):
-            os.mkdir(absolute_path)
-        if not os.path.exists(model1_path):
-            os.mkdir(model1_path)
-        if not os.path.exists(model2_path):
-            os.mkdir(model2_path)
-        if not os.path.exists(model3_path):
-            os.mkdir(model3_path)
-        if not os.path.exists(model4_path):
-            os.mkdir(model4_path)
-        if not os.path.exists(model1_waterfall):
-            os.mkdir(model1_waterfall)
-        if not os.path.exists(model2_waterfall):
-            os.mkdir(model2_waterfall)
-        if not os.path.exists(model3_waterfall):
-            os.mkdir(model3_waterfall)
-        if not os.path.exists(model4_waterfall):
-            os.mkdir(model4_waterfall)
-
+        os.mkdir(absolute_path)
     except OSError:
-        logging.info("Creation of the directory %s failed. Folder already exists." % absolute_path)
-        # try:
+        logging.error("Creation of the directory %s failed. Folder already exists." % absolute_path)
     else:
         logging.info("Successfully created the directory %s " % absolute_path)
 
 
 def move_results():
     cwd = os.getcwd()
-    dst_dir = cwd + "/models_results"
+    dst_dir = cwd + "/Output/models_results"
     try:
         shutil.move(os.path.join(cwd, 'SciKit_Model_Results.txt'),
                     os.path.join(dst_dir, 'SciKit_Model_Results.txt'))
@@ -65,11 +38,20 @@ if __name__ == "__main__":
     try:
         current_dir = os.getcwd()
         script_dir = current_dir + '/SciKit_Scripts'
+
         # Get the data from the argument
         file_name = sys.argv[1]
+
         logging.info(f'Loading Data {file_name}\n')
-        set_directory()
-        scripts = ['/BR.py', '/LR.py', '/RR.py', '/RFR.py']
+
+        set_directory('/Output')
+        set_directory('/Output/models_results')
+
+        for i in range(1, 6):
+            set_directory(f'/Output/models_results/scikit_model_{i}')
+            set_directory(f'/Output/models_results/scikit_model_{i}/waterfalls')
+
+        scripts = ['/BR.py', '/LR.py', '/RR.py', '/RFR.py', '/CB.py']
 
         for script in scripts:
             os.system("python " + '"' + script_dir + '"' + script + f" {file_name}")
